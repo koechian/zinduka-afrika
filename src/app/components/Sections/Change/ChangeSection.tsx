42240ed32deb5661a7dcd9341ee48387b9616ea0
koechian <ianovic8@live.com>
2023-04-15 11:31:29 +0300

    Done changes page

--- src/app/components/Sections/Change/ChangeSection.tsx
+++ src/app/components/Sections/Change/ChangeSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
+import MyButton from "../../Utils/button/Button";
 
 const ChangeSection = () => {
   return (
@@ -7,10 +8,42 @@ const ChangeSection = () => {
       className={styles.wrapper}
       style={{ backgroundImage: "url(/change.svg)" }}
     >
-      <p>
-        Join us and be part of a positive change in the world. <br />
-        With over:
-      </p>
+      <div className={[styles.left, styles.imageContainers].join(" ")}>
+        <div
+          className={styles.imageLarge}
+          style={{ backgroundImage: "url(/zinduka5.jpg)" }}
+        ></div>
+        <div
+          className={styles.imageSmall}
+          style={{
+            backgroundImage: "url(/zinduka4.jpg)",
+          }}
+        ></div>
+      </div>
+      <div className={styles.center}>
+        <p>
+          Join us and be part of a positive change in the world. <br />
+          With over:
+        </p>
+        <div>
+          <span>129,355+</span>
+        </div>
+        <p>Other like minded mentors, donors, leaders, speakers & sponsors</p>
+        <MyButton type={"Primary"} title={"Sign Me Up!"} />
+      </div>
+      <div className={[styles.right, styles.imageContainers].join(" ")}>
+        <div
+          className={styles.imageLarge}
+          style={{ alignSelf: "flex-start", backgroundImage: "url(/ball.jpg)" }}
+        ></div>
+        <div
+          className={styles.imageSmall}
+          style={{
+            alignSelf: "flex-start",
+            backgroundImage: "url(/zinduka3.jpg)",
+          }}
+        ></div>
+      </div>
     </section>
   );
 };
--- src/app/components/Utils/button/Button.tsx
+++ src/app/components/Utils/button/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./button.module.css";
+import { inter } from "@/app/fonts";
 
 interface ButtonOptions {
   title: String;
@@ -10,13 +11,25 @@ const MyButton = ({ type, title }: ButtonOptions) => {
   switch (type as String) {
     case "Primary":
       return (
-        <button className={[styles.primaryButton, styles.button].join(" ")}>
+        <button
+          className={[
+            styles.primaryButton,
+            styles.button,
+            inter.className,
+          ].join(" ")}
+        >
           {title}
         </button>
       );
     case "Secondary":
       return (
-        <button className={[styles.secondaryButton, styles.button].join(" ")}>
+        <button
+          className={[
+            inter.className,
+            styles.secondaryButton,
+            styles.button,
+          ].join(" ")}
+        >
           {title}
         </button>
       );
