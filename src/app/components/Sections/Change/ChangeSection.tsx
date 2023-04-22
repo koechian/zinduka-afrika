5ddd8e92acf97b4bfc925a865e46e18918861dbf
koechian <ianovic8@live.com>
2023-04-22 23:12:06 +0300

    Change section now responsive

--- src/app/components/Sections/Change/ChangeSection.tsx
+++ src/app/components/Sections/Change/ChangeSection.tsx
@@ -8,41 +8,46 @@ const ChangeSection = () => {
       className={styles.wrapper}
       style={{ backgroundImage: "url(/change.svg)" }}
     >
-      <div className={[styles.left, styles.imageContainers].join(" ")}>
-        <div
-          className={styles.imageLarge}
-          style={{ backgroundImage: "url(/zinduka5.jpg)" }}
-        ></div>
-        <div
-          className={styles.imageSmall}
-          style={{
-            backgroundImage: "url(/zinduka4.jpg)",
-          }}
-        ></div>
-      </div>
-      <div className={styles.center}>
-        <p>
-          Join us and be part of a positive change in the world. <br />
-          With over:
-        </p>
-        <div>
-          <span>129,355+</span>
+      <div className={styles.container}>
+        <div className={[styles.left, styles.imageContainers].join(" ")}>
+          <div
+            className={styles.imageLarge}
+            style={{ backgroundImage: "url(/zinduka5.jpg)" }}
+          ></div>
+          <div
+            className={styles.imageSmall}
+            style={{
+              backgroundImage: "url(/zinduka4.jpg)",
+            }}
+          ></div>
         </div>
-        <p>Other like minded mentors, donors, leaders, speakers & sponsors</p>
-        <MyButton type={"Primary"} title={"Sign Me Up!"} />
-      </div>
-      <div className={[styles.right, styles.imageContainers].join(" ")}>
-        <div
-          className={styles.imageLarge}
-          style={{ alignSelf: "flex-start", backgroundImage: "url(/ball.jpg)" }}
-        ></div>
-        <div
-          className={styles.imageSmall}
-          style={{
-            alignSelf: "flex-start",
-            backgroundImage: "url(/zinduka3.jpg)",
-          }}
-        ></div>
+        <div className={styles.center}>
+          <p>
+            Join us and be part of a positive change in the world. <br />
+            With over:
+          </p>
+          <div>
+            <span>129,355+</span>
+          </div>
+          <p>Other like minded mentors, donors, leaders, speakers & sponsors</p>
+          <MyButton type={"Primary"} title={"Sign Me Up!"} />
+        </div>
+        <div className={[styles.right, styles.imageContainers].join(" ")}>
+          <div
+            className={styles.imageLarge}
+            style={{
+              alignSelf: "flex-start",
+              backgroundImage: "url(/ball.jpg)",
+            }}
+          ></div>
+          <div
+            className={styles.imageSmall}
+            style={{
+              alignSelf: "flex-start",
+              backgroundImage: "url(/zinduka3.jpg)",
+            }}
+          ></div>
+        </div>
       </div>
     </section>
   );
