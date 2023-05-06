c03da77356fe013df3227e5eeb3f97daaa8b5335
koechian <ianovic8@live.com>
2023-05-06 15:07:06 +0300

    Banner moves on scroll

--- src/app/components/Utils/banner/Banner.tsx
+++ src/app/components/Utils/banner/Banner.tsx
@@ -1,20 +1,54 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import styles from "./banner.module.css";
+import gsap from "gsap";
 
 const Banner = () => {
+  //   animations
+  const topBanner = useRef<HTMLDivElement>(null);
+  const bottomBanner = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      // @ts-ignore
+      gsap.to(topBanner.current.children, {
+        duration: 0.2,
+        x: -scrollPosition / 6,
+        ease: "power1.inOut",
+      });
+      // @ts-ignore
+      gsap.to(bottomBanner.current.children, {
+        duration: 0.2,
+        //   @ts-ignore
+        x: (scrollPosition - bottomBanner.current.offsetTop) / 6,
+        ease: "power1.inOut",
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll);
+  });
+
   const spans: any = [];
-  for (let index = 0; index < 20; index++) {
+  for (let index = 0; index < 100; index++) {
     spans[index] = "arise, change the world * * ";
   }
 
   return (
     <div className={styles.container}>
-      <div className={[styles.topBanner, styles.banner].join(" ")}>
+      <div
+        ref={topBanner}
+        className={[styles.topBanner, styles.banner].join(" ")}
+      >
         {spans.map((span: string, index: number) => (
           <span key={index}> {span}</span>
         ))}
       </div>
-      <div className={[styles.bottomBanner, styles.banner].join(" ")}>
+      <div
+        ref={bottomBanner}
+        className={[styles.bottomBanner, styles.banner].join(" ")}
+      >
         <span>the world * *</span>
         {spans.map((span: string, index: number) => (
           <span key={index}> {span}</span>
