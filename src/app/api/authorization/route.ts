473c83023a13302999397673098d9778de9dbdf7
Ian Koech <ianovic8@live.com>
2024-03-17 13:42:44 +0300

    Safaricom API Authorization

---
+++ src/app/api/authorization/route.ts
@@ -0,0 +1,31 @@
+export async function POST(request: Request) {
+  let code = encoder();
+  try {
+    const response = await fetch(
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Basic " + encoder(),
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log(response.text);
+    }
+
+    return new Response(JSON.stringify(await response.text()), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to auth", { status: 400 });
+  }
+}
+
+function encoder() {
+  console.log(process.env.KEY);
+  return Buffer.from(
+    process.env.KEY + ":" + process.env.SECRET,
+    "utf-8"
+  ).toString("base64");
+}
